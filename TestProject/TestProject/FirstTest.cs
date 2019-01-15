03340854bc43e3d4c191409f865eda981ee462f3
olidro2907 <45629063+olidro2907@users.noreply.github.com>
2019-01-15 01:28:36 +0100

    Przegladarka_Chrome

---
+++ TestProject/TestProject/FirstTest.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Opera;
+using OpenQA.Selenium.IE;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ChromeMethod()
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\Oliwia Drozdek\Documents\GitHub\Testowanie_ekartorteki\TestProject\TestProject\bin\Debug\netcoreapp2.1");
+            driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Window.FullScreen();
+            //driver.SwitchTo().Window("Zaloguj siê");
+
+
+            //driver.Close();
+
+        }
+
+        //[Test]
+        //public void IEMethod()
+        //{
+        //    IWebDriver driver = new InternetExplorerDriver();
+        //    driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
+        //    driver.Manage().Window.Maximize();
+        //    driver.Close();
+
+        //}
+
+        //[Test]
+        //public void OperaMethod()
+        //{
+        //    IWebDriver driver = new OperaDriver();
+        //    driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
+        //    driver.Manage().Window.Maximize();
+        //    driver.Close();
+
+        //}
+
+        //[Test]
+        //public void FirefoxMethod()
+        //{
+        //    IWebDriver driver = new FirefoxDriver();
+        //    driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
+        //    driver.Manage().Window.Maximize();
+        //    driver.Close();
+
+        //}
+    }
+}
