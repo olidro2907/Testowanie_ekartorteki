353e5179509df56acf91c1a42e00b78a40dc9395
olidro2907 <45629063+olidro2907@users.noreply.github.com>
2019-01-15 10:56:56 +0100

    Wstępne_testy_na_przeglądarkach

--- TestProject/TestProject/FirstTest.cs
+++ TestProject/TestProject/FirstTest.cs
@@ -21,41 +21,41 @@ namespace Tests
             driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
             driver.Manage().Window.Maximize();
             driver.Manage().Window.FullScreen();
-            //driver.SwitchTo().Window("Zaloguj siê");
+            //driver.SwitchTo().Window("Logowanie - Strona");
 
 
             //driver.Close();
 
         }
 
-        //[Test]
-        //public void IEMethod()
-        //{
-        //    IWebDriver driver = new InternetExplorerDriver();
-        //    driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
-        //    driver.Manage().Window.Maximize();
-        //    driver.Close();
+        [Test]
+        public void IEMethod()
+        {
+            IWebDriver driver = new InternetExplorerDriver(@"C:\Users\Oliwia Drozdek\Documents\GitHub\Testowanie_ekartorteki\TestProject\TestProject\bin\Debug\netcoreapp2.1");
+            driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
+            driver.Manage().Window.Maximize();
+            driver.Close();
 
-        //}
+        }
 
-        //[Test]
-        //public void OperaMethod()
-        //{
-        //    IWebDriver driver = new OperaDriver();
-        //    driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
-        //    driver.Manage().Window.Maximize();
-        //    driver.Close();
+        [Test]
+        public void OperaMethod()
+        {
+            IWebDriver driver = new OperaDriver(@"C:\Users\Oliwia Drozdek\Documents\GitHub\Testowanie_ekartorteki\TestProject\TestProject\bin\Debug\netcoreapp2.1");
+            driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
+            driver.Manage().Window.Maximize();
+            driver.Close();
 
-        //}
+        }
 
-        //[Test]
-        //public void FirefoxMethod()
-        //{
-        //    IWebDriver driver = new FirefoxDriver();
-        //    driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
-        //    driver.Manage().Window.Maximize();
-        //    driver.Close();
+        [Test]
+        public void FirefoxMethod()
+        {
+            IWebDriver driver = new FirefoxDriver(@"C:\Users\Oliwia Drozdek\Documents\GitHub\Testowanie_ekartorteki\TestProject\TestProject\bin\Debug\netcoreapp2.1");
+            driver.Navigate().GoToUrl("https://ekartoteka.azurewebsites.net/?fbclid=IwAR1PCdfWuVGB-KeHWhwJ8DImd2rJ2AtxEK2M2AXdv2YNhXhXLRegRp3No4w");
+            driver.Manage().Window.Maximize();
+            driver.Close();
 
-        //}
+        }
     }
 }
